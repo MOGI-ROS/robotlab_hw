a7592d44e7ed92cee7b2d4f70fd7eab821a887ce
PhylMacao <13609252+PhylMacao@users.noreply.github.com>
2021-05-20 02:07:43 +0200

    made multi-goal navigation

--- scripts/path_publisher.py
+++ scripts/path_publisher.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import rospy
+import tf
 import actionlib
+import numpy
 
 from std_msgs.msg import String, Empty
 from geometry_msgs.msg import PoseStamped
@@ -10,34 +12,48 @@ from actionlib_msgs.msg import GoalStatus
 from geometry_msgs.msg import Pose, Point, Quaternion
 from tf.transformations import quaternion_from_euler
 
+pi = 3.14159
+
 def path_publisher():
     rospy.init_node('path_publisher')
     rospy.loginfo("Node initialized.")
+    base_point = numpy.array([[-7.5, 3.1, pi]])
+    table_1 = numpy.array([[-5.5, 2.1, pi/2]])
+    table_2 = numpy.array([[-3.75, 2.1, pi/2]])
+    goals = numpy.vstack(
+        (base_point,
+        table_1,
+        table_2,
+        base_point)
+    )
+
     client = actionlib.SimpleActionClient('robot/move_base',MoveBaseAction)
     rospy.loginfo("Connecting to move_base...")
     client.wait_for_server()
     rospy.loginfo("Connected to move_base.")
 
-    goal = MoveBaseGoal()
-    goal.target_pose.header.frame_id = "robot_map"
-    goal.target_pose.header.stamp = rospy.Time.now()
+    for i in range(0, goals.shape[0]):
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = "robot_map"
+        goal.target_pose.header.stamp = rospy.Time.now()
 
-    goal.target_pose.pose.position.x = -0.781250178814
-    goal.target_pose.pose.position.y = 1.52264034748
-    goal.target_pose.pose.position.z = 0.0
-    goal.target_pose.pose.orientation.x = 0.0
-    goal.target_pose.pose.orientation.y = 0.0
-    goal.target_pose.pose.orientation.z = 0.0
-    goal.target_pose.pose.orientation.w = 1.0
+        goal.target_pose.pose.position.x = goals[i][0]
+        goal.target_pose.pose.position.y = goals[i][1]
+        goal.target_pose.pose.position.z = 0.0
+        goal.target_pose.pose.orientation = Quaternion(*tf.transformations.quaternion_from_euler(0, 0, goals[i][2]))
+
+        rospy.loginfo("Sending goal...")
+        client.send_goal(goal)
+        client.wait_for_result()
     
-    client.send_goal(goal)
-    client.wait_for_result()
-
-    if client.get_state() == GoalStatus.SUCCEEDED:
-        rospy.loginfo("Goal reached.")
-    else:
-        rospy.loginfo("Failed to reach goal.")
-        rospy.loginfo("Status code: " + str(client.get_state()))
+        if client.get_state() == GoalStatus.SUCCEEDED:
+            rospy.loginfo("Goal reached.")
+        else:
+            rospy.loginfo("Failed to reach goal.")
+            rospy.loginfo("Status code: " + str(client.get_state()))
+        rospy.loginfo("Waiting before sending next goal...")
+        rospy.sleep(3)
+    rospy.loginfo("Reached end of goals.")
 
 
 if __name__ == '__main__':
